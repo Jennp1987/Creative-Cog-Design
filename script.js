48eed28f59ee391605f13324d9576fb5c928cde9
Jennp1987 <44036108+Jennp1987@users.noreply.github.com>
2020-07-08 11:56:59 -0700

    update styles

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 /*eslint-env browser*/
-var backgroundImg = new Image();
-backgroundImg.src = 'https://jennp1987.github.io/Creative-Cog-Design/images/creativeCogHeaderNewpng825.png';
+// var backgroundImg = new Image();
+// backgroundImg.src = 'https://jennp1987.github.io/Creative-Cog-Design/images/creativeCogHeaderNewpng825.png';
